7866c11803428db840ffe19dd2ac67220f9ac1bc
Hiro Mitsui <hiroshimitsui94@gmail.com>
2017-03-05 10:46:14 -0300

    Creando Menú

--- MyFristApp/app/src/main/java/com/example/fpuna/myfirstapp/MyFirstApp.java
+++ MyFristApp/app/src/main/java/com/example/fpuna/myfirstapp/MyFirstApp.java
@@ -1,5 +1,6 @@
 package com.example.fpuna.myfirstapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +10,22 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class MyFirstApp extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
 
     private LinearLayout Seccion_Persona;
     private Button SignOut;
-    private SignInButton SigIn;
+    private SignInButton SignIn;
     private TextView Name,Email;
     private ImageView Android_Pic;
     private GoogleApiClient googleApiCliente;
@@ -28,17 +36,104 @@ public class MyFirstApp extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_first_app);
-        Seccion_Persona = (LinearLayout)findViewById(R.id.seccion_persona);
+        Seccion_Persona = (LinearLayout)findViewById(R.id.section_persona);
+        SignOut = (Button)findViewById(R.id.button_LogOut);
+        SignIn = (SignInButton) findViewById(R.id.button_LogIn);
+        Name = (TextView)findViewById(R.id.name);
+        Email = (TextView)findViewById(R.id.email);
+        Android_Pic = (ImageView)findViewById(R.id.android_pic);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Seccion_Persona.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiCliente = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+
 
     }
 
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.button_LogIn:
+                signIn();
+                break;
+            case R.id.button_LogOut:
+                signOut();
+                break;
+
+        }
+
+
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiCliente);
+        startActivityForResult(intent,REQ_CODE);
+
+
+    }
+    private void signOut()
+    {
+        Auth.GoogleSignInApi.signOut(googleApiCliente).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+
+    }
+    private void handleResult(GoogleSignInResult result)
+    {
+        if(result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_url = account.getPhotoUrl().toString();
+            Name.setText(name);
+            Email.setText(email);
+            Glide.with(this).load(img_url).into(Android_Pic);
+            updateUI(true);
+        }
+         else
+        {
+            updateUI(false);
+        }
+
+
+
+    }
+
+    private void updateUI(boolean isLogin)
+    {
+        if(isLogin)
+        {
+            Seccion_Persona.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+
+        }else
+        {
+            Seccion_Persona.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+
+        }
     }
 }
