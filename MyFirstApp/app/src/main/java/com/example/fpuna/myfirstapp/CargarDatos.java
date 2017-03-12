89019b9ce2a46efda8abd1e46c98d914aa0c0da2
HiroMitsui <hiroshimitsui94@gmail.com>
2017-03-12 15:02:45 -0300

    Menu Usuario

---
+++ MyFirstApp/app/src/main/java/com/example/fpuna/myfirstapp/CargarDatos.java
@@ -0,0 +1,13 @@
+package com.example.fpuna.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CargarDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargar_datos);
+    }
+}
