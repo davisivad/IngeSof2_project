e5208e13f6c9a053d370359187dee2c2533f2bf4
cynmorel <cyn.morel@gmail.com>
2017-03-12 10:53:13 -0300

    Menu Usuario

---
+++ MyFirstApp/app/src/main/java/com/example/fpuna/myfirstapp/CargaDatos.java
@@ -0,0 +1,13 @@
+package com.example.fpuna.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CargaDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga_datos);
+    }
+}
